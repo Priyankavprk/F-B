67c346bf94d1f1f1eb82f8200bcd8b9fdcc651a2
priyanka <priyankap@rigbot.com>
2019-06-20 11:53:07 +0530

    Add highlight to selected tab

--- app/App.js
+++ app/App.js
@@ -21,20 +21,26 @@ export default class App extends Component {
     })
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.tabOnView !== nextState.tabOnView) {
+      return true
+    }
+    return false
+  }
+
   render() {
+    console.log(';;;;;;',this.state.tabOnView)
     return (
       <View style={styles.container}>
         <Text style={{fontSize: 30, color: '#FFF', fontWeight: 'bold', margin: 20}}> F & B </Text>
         <View  style={{borderColor: '#C7CACB', width: width - 20, borderWidth: 1}}/>
           <View style={styles.tab}>
-            <TouchableOpacity style={styles.tabStyle} onPress={() => this.tabOnChange(data.FoodList[0].TabName)}><Text style={{fontSize: 20, color: '#FFF'}}>{data.FoodList[0].TabName}</Text></TouchableOpacity>
-            <View style={{borderLeftWidth: 1, borderLeftColor: 'white'}}/>
-            <TouchableOpacity style={styles.tabStyle} onPress={() => this.tabOnChange(data.FoodList[1].TabName)}><Text style={{fontSize: 20, color: '#FFF'}}>{data.FoodList[1].TabName}</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.tabStyle} onPress={() => this.tabOnChange(data.FoodList[0].TabName)}><Text style={{fontSize: 20, color: this.state.tabOnView === data.FoodList[0].TabName ? '#FFF' : '#B8BABB', fontWeight: this.state.tabOnView === data.FoodList[0].TabName ? 'bold' : null}}>{data.FoodList[0].TabName}</Text></TouchableOpacity>
+            <View style={{borderLeftWidth: 1, borderLeftColor: '#C7CACB', height: 20, marginTop: 25}}/>
+            <TouchableOpacity style={styles.tabStyle} onPress={() => this.tabOnChange(data.FoodList[1].TabName)}><Text style={{fontSize: 20, color: this.state.tabOnView === data.FoodList[1].TabName ? '#FFF' : '#B8BABB', fontWeight: this.state.tabOnView === data.FoodList[1].TabName ? 'bold' : null}}>{data.FoodList[1].TabName}</Text></TouchableOpacity>
           </View>
           <ScrollView>
-
             <Card cardItems={data.FoodList.filter((item) => item.TabName === this.state.tabOnView)}/>
-
           </ScrollView>
       </View>
     );
